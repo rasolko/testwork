a8aea22bb80aa8417f7b9c4788d670e704aff8b5
rasolko <53225601+rasolko@users.noreply.github.com>
2020-08-28 19:05:47 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const onCheckArrow2 = () => {
+    document.getElementById('arrow4').checked = false;
+    document.getElementById('el2').scrollIntoView();
+}
+const onCheckArrow4 = () => {
+    document.getElementById('arrow2').checked = false;
+    document.getElementById('el4').scrollIntoView();
+}
+const onCheckArrow2_2 = () => {
+    document.getElementById('arrow2_4').checked = false;
+    document.getElementById('el2_2').scrollIntoView();
+}
+const onCheckArrow2_4 = () => {
+    document.getElementById('arrow2_2').checked = false;
+    document.getElementById('el2_4').scrollIntoView();
+}
